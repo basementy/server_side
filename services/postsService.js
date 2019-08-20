f23f7707ab2e5c3e8e40d3a5287873f76a77f47c
GABRIEL DE OLIVEIRA <gabriel99.oliveira@catolicasc.edu.br>
2019-08-20 20:20:07 -0300

    post data alterations

--- services/postsService.js
+++ services/postsService.js
@@ -1,24 +1,28 @@
 var getPosts = function() {
   var posts = [
     {
+      id: 1,
       title: 'Post 1',
       image: 'post1.jpg',
       description: 'Meu primeiro post',
       body: 'Meu primeiro post blablabla'
     },
     {
+      id: 2,
       title: 'Post 2',
       image: 'post1.jpg',
       description: 'Meu segundo post',
       body: 'Meu segundo post blablabla'
     },
     {
+      id: 3,
       title: 'Post 3',
       image: 'post1.jpg',
       description: 'Meu terceiro post',
       body: 'Meu terceiro post blablabla'
     },
     {
+      id: 4,
       title: 'Post 4',
       image: 'post1.jpg',
       description: 'Meu quarto post',
