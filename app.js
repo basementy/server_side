ab16b3522033e6cf5560305e108c57071495fb73
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-09-11 19:54:12 -0300

    Created the testimonial route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testimonialsRouter = require('./routes/testimonials');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/testimonials', testimonials)
+app.use('/testimonials', testimonialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('./components/error');
 });
 
 module.exports = app;
