b1c1a689a69c2897ae1b0255b13e720d5d275a43
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-09-24 20:51:07 -0300

    Setting new route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testimonialsRouter = require('./routes/testimonials');
 var servicesRouter = require('./routes/services');
+var adminRouter = require('./routes/admin/posts');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testimonials', testimonialsRouter);
 app.use('/services', servicesRouter);
+app.use('/admin/posts', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
