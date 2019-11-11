510afb283b476a47250cc80868eee05f3eb27cd9
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-11-11 01:49:06 -0300

    New routes

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 // Start
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var session = require("express-session");
+var createError = require("http-errors")
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
+var session = require("express-session")
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testimonialsRouter = require("./routes/testimonials");
-var servicesRouter = require("./routes/services");
-var adminPostsRouter = require("./routes/admin/posts");
-var adminTestimonialsRouter = require("./routes/admin/testimonials");
-var adminProjectsRouter = require("./routes/admin/projects");
-var authRouter = require("./routes/auth");
-var verifyAuth = require("./middlewares/authMiddleware");
+var indexRouter = require("./routes/index")
+var usersRouter = require("./routes/users")
+var testimonialsRouter = require("./routes/testimonials")
+var servicesRouter = require("./routes/services")
 
-var app = express();
+var adminPostsRouter = require("./routes/admin/posts")
+var adminTestimonialsRouter = require("./routes/admin/testimonials")
+var adminProjectsRouter = require("./routes/admin/projects")
 
-app.set("trust proxy", 1);
+var authRouter = require("./routes/auth")
+var verifyAuth = require("./middlewares/authMiddleware")
+var devicesRouter = require('./routes/devices')
+
+var app = express()
+
+app.set("trust proxy", 1)
 app.use(
   session({
     secret: "mySecret123",
@@ -26,42 +29,44 @@ app.use(
     saveUninitialized: true,
     cookie: { secure: false }
   })
-);
+)
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/public", express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter);
-app.use('/auth', authRouter);
-app.use("/users", usersRouter);
-app.use("/testimonials", testimonialsRouter);
-app.use("/services", servicesRouter);
-app.use("/admin/posts", [verifyAuth], adminPostsRouter);
-app.use("/admin/testimonials", [verifyAuth], adminTestimonialsRouter);
-app.use("/admin/projects", [verifyAuth], adminProjectsRouter);
+app.use("/", indexRouter)
+app.use('/auth', authRouter)
+app.use("/users", usersRouter)
+app.use("/testimonials", testimonialsRouter)
+app.use("/services", servicesRouter)
+app.use('/devices', devicesRouter);
+
+app.use("/admin/posts", [verifyAuth], adminPostsRouter)
+app.use("/admin/testimonials", [verifyAuth], adminTestimonialsRouter)
+app.use("/admin/projects", [verifyAuth], adminProjectsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("./components/error");
-});
+  res.status(err.status || 500)
+  res.render("./components/error")
+})
 
-module.exports = app;
+module.exports = app
